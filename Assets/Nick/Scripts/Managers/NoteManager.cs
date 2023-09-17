e63bada361dd534ac069ea2ad96ebfc3f8892c7d
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-09-17 15:53:12 -0400

    Implementing Sprite Animations, Changing Backgrounds

--- Assets/Nick/Scripts/Managers/NoteManager.cs
+++ Assets/Nick/Scripts/Managers/NoteManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7870495bdc20659c7b223bda68e66f36cbe2e42b1cff4d8f76bb2297895fc778
-size 3858
+oid sha256:5b2f2feea0c0583a5731e4ecde2d7af6614d5f8ded97726916f189353d6edaa1
+size 3860
