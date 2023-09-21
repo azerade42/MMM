cd2fad06a7ca3325edf725c95689a97854c6203e
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-09-20 23:30:47 -0400

    Closing windows, fixing player jitter

--- Assets/Nick/Scripts/Gameplay/PlayerInput.cs
+++ Assets/Nick/Scripts/Gameplay/PlayerInput.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02cf018c87bbd99494e6db71767a2ffe0a373a9bc25c59df232592d125945122
-size 2906
+oid sha256:0275360b2f84ac26752bdc4559ddfb4ce2503f5ff817b7e3374a4e0617d0b9fd
+size 3027
---
+++ Assets/Nick/Scripts/Gameplay/Window.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e7913452f3bccd6d416129d9ef55b3ec6433f89065334e1e4ead9ab0b2731cd5
+size 372
