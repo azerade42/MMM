c2cfbd24366fe891086dc379d63f4b7e81936ca6
GabrielFernandez14 <79731548+GabrielFernandez14@users.noreply.github.com>
2023-09-21 10:19:27 -0400

    Button Functionality and Audio
    Still need to setup audio manager and everything.

--- Assets/Gabriel/Scripts/MainMenu.cs
+++ Assets/Gabriel/Scripts/MainMenu.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void PlayGame()
+    {
+        // Change this to looking up scene by name
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
---
+++ Assets/Gabriel/Scripts/SaveValues.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveValues
+{
+    public static float musicVolume = 0.2f;
+    public static float sfxVolume = 0.2f;
+    public static float spookyPercent = 0.5f;
+
+    public static bool isFullscreen = true;
+    public static bool isGodMode = false;
+}
