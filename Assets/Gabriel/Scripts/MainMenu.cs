acea4d097c51f16a4c3ad088a0e4680d4511be0f
LagSpikeStorm <112224836+LagSpikeStorm@users.noreply.github.com>
2023-09-24 15:17:44 -0400

    End Menu

--- Assets/Gabriel/Scripts/MainMenu.cs
+++ Assets/Gabriel/Scripts/MainMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40af8501752e44ff015da5e96486d3e75bda9b267661e146abc718e4469dfb4a
-size 384
+oid sha256:2f4cb90faa8b977d0e2081da711a718afd1a4204b761e7afcb28eafe05661d44
+size 678
