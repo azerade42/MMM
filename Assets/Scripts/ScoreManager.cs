dd0e5eb78926a4f04226e44c1871091a45476b25
LagSpikeStorm <112224836+LagSpikeStorm@users.noreply.github.com>
2023-09-27 14:58:17 -0400

    Messing Around with Score Manager and Main Menu

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class ScoreManager : MonoBehaviour
@@ -10,13 +12,18 @@ public class ScoreManager : MonoBehaviour
     public AudioSource hitSFX;
     public AudioSource missSFX;
     public TextMeshPro scoreText;
-    
+    public Slider healthSlider;
     public GameObject endScreen;
+    public GameObject loseScreen;
     public TextMeshProUGUI menuScore;
+    public float rateOfChange;
+    public int healthLoss;
+    
+
     static int comboScore;
     static float scoreTrack;
     static int meterScore;
-    public float rateOfChange;
+    static int health;
    
     void Start()
     {
@@ -24,17 +31,21 @@ public class ScoreManager : MonoBehaviour
         comboScore = 0;
         scoreTrack = 0;
         meterScore = 0;
+        health = 50;
+        healthSlider.value = health;
     }
     public static void PerfectHit()
     {
         comboScore += 250;
         meterScore += 1;
+        health += meterScore / 2;
         Instance.hitSFX.Play();
     }
     public static void GoodHit()
     {
         comboScore += 100;
         meterScore += 1;
+        health += meterScore / 2;
         Instance.hitSFX.Play();
     }
     public static void Miss()
@@ -44,15 +55,28 @@ public class ScoreManager : MonoBehaviour
         else
             comboScore = 0;
         meterScore = 0;
+        health -= 5;
         Instance.missSFX.Play();    
     }
     private void Update()
     {
+        if (health >= 100)
+            health = 100;
+        else if (health <= 0)
+            health = 0;
+        healthSlider.value = health;
         scoreText.text = meterScore.ToString();
         if (endScreen.activeSelf && scoreTrack < comboScore)
         {
             scoreTrack += rateOfChange * Time.deltaTime;
             menuScore.text = scoreTrack.ToString("F0");
+        }
+        if(healthSlider.value <= 0)
+        {
+            loseScreen.SetActive(true);
+            health = 50;
+            healthSlider.value = health;
+            GameManager.TriggerPause();
         }
     }
 }
