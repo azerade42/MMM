4fe47f549214b457467081f7759b62a798cc9666
Nick <56888291+nhussian42@users.noreply.github.com>
2023-10-03 12:23:28 -0400

    nightclub level screens

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27ac972aa4171af2d409af6934815be2736212490de676c637a52d3e39d6c980
-size 2131
+oid sha256:0dbb82776defbd46118e59125ad60ec8f628065e7512a0624458f1eae3199f8d
+size 2135
