0cbd31662e22467e65df46be71038562b4abe780
Nick <56888291+nhussian42@users.noreply.github.com>
2023-10-05 10:16:52 -0400

    smallchange

--- Assets/Scripts/Managers/LaneManager.cs
+++ Assets/Scripts/Managers/LaneManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7590c651b6e7f1582cabe231fbd37748ac34c99e165f6bc8c11ae551f73ceb64
-size 10219
+oid sha256:ab9f747f09ddfd7a12aa99547edc65277fa33e44ac0fa088f401987dae127670
+size 10310
