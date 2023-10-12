b5f5c5dcc376f31957696f5d9ba7cfb9bc3f7ee8
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-10-11 20:53:36 -0400

    Animating background objects on beat

--- Assets/Scripts/Managers/LaneManager.cs
+++ Assets/Scripts/Managers/LaneManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abdefd02d6d8ca6526005fef363d27f8a70e6c532f5b5c64bc6ebe21cfa74d26
-size 15754
+oid sha256:2244d79d168816095c2b06a800527a2a5fa900d2957018fc38793757adf4c516
+size 16269
