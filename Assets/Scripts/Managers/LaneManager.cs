3d5d6221f8e8a6e1fb681e6c79f78be72458a071
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-10-18 14:00:06 -0400

    fixing up nightclub timing

--- Assets/Scripts/Managers/LaneManager.cs
+++ Assets/Scripts/Managers/LaneManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:036c1e49f99e7b8ef790003edc0e792fe9306b3daf08d5a57fc8b578f6d67546
-size 19610
+oid sha256:3b195538ec4244c35f52f6fdfd875af614ca7c04c5f8396badf3792ab9032270
+size 19611
