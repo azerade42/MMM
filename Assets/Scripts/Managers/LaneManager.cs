a0cee753a8f530d1e2b9fefe09d101bd560be715
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-10-18 00:05:35 -0400

    fixing end screen kinda need to fix score

--- Assets/Scripts/Managers/LaneManager.cs
+++ Assets/Scripts/Managers/LaneManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddbb45ae452af3430b8a73c7019d3771d29437d3186b3b7ca00f66ba6b55dd4b
-size 18581
+oid sha256:9ced188ea199704bb0ab331388d482ea9c1eacbdcffa085cf8240f2c946b3397
+size 19358
