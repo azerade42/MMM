b8e1e14d72c0cb4478ee90f0d679a5616648cc11
LagSpikeStorm <112224836+LagSpikeStorm@users.noreply.github.com>
2023-10-25 20:23:00 -0400

    Added ScreenMoveTest and related Scripts

---
+++ Assets/Scripts/Managers/ScreenMoveLeft.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenMoveLeft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10f;
+    Vector3 startPos;
+    float repeatWidth;
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider2D>().size.x/2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left *  Time.deltaTime  * speed);
+        if(transform.position.x < startPos.x - repeatWidth ){
+            transform.position = startPos;
+        }
+    }
+}
