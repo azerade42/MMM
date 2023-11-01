bf37b5f77fdcd8d09f09a378c3b7ca78ffa2e9a7
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-01 15:13:29 -0400

    update

--- Assets/Scripts/Gameplay/Transition/TransitionPlayerController.cs
+++ Assets/Scripts/Gameplay/Transition/TransitionPlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da02cfaadad931031ab2cab58dee3a7daa7dc635a8d343d634d47051f8f72776
-size 3925
+oid sha256:2c87ec9a575b3086d6a80722c642c142ba3b4a4e8c5a57a5413f61002c9a50d7
+size 3680
