a1af82ae36d019098801b879ef2401f05f444016
GabrielFernandez14 <gabrielfernandez14213@gmail.com>
2023-11-07 00:56:16 -0500

    Set Up Basic 3rd Level Layout
    - AudioManager updated with new song
    - Build Settings Order Fixed
    - SongManager added new song

--- Assets/Scripts/Managers/SongManager.cs
+++ Assets/Scripts/Managers/SongManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8b64c28e480f51b3daaaecd785281ca1c2ee06d9aabedb36344a14fb1b345d8
-size 3662
+oid sha256:cdda55aed36e2ab4cbdf90617e00a3666f79a8c76ec4964f82c4fcf0e4a99808
+size 3769
