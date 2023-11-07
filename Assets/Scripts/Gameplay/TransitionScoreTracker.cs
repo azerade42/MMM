e3d0bcb5eb54d79d37a929c230b875af93b9aaf6
LagSpikeStorm <112224836+LagSpikeStorm@users.noreply.github.com>
2023-11-07 13:36:14 -0500

    Added Floating Screens

--- Assets/Scripts/Gameplay/TransitionScoreTracker.cs
+++ Assets/Scripts/Gameplay/TransitionScoreTracker.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e617a46c772f6769de5862ea979c4952aa8e6f00eca7825d7072d64a6e70385
-size 627
+oid sha256:8061f2b071fe926fef038265208994119581ee9faefb8928bc3fa1e6de86391d
+size 629
