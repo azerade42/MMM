8e09055adeb9e389feafe6d232f871e7dff4eb18
Nicholas <56888291+nhussian42@users.noreply.github.com>
2023-11-08 14:15:58 -0500

    blinking when hit + slow down

--- Assets/Scripts/Gameplay/Transition/TransitionPlayerController.cs
+++ Assets/Scripts/Gameplay/Transition/TransitionPlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d386d040b743ec5a535bb4f3902cd795143da5eef3281d7482b83d78c29223b4
-size 4437
+oid sha256:443c092351f6c309d2632b16d8d0661c7ae6b9c7b30a7b50e8c762b1031d8fac
+size 5983
