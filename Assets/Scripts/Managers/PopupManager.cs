3ce48652619d5a5075e35947fe7c030f2965d836
LagSpikeStorm <112224836+LagSpikeStorm@users.noreply.github.com>
2023-11-16 02:27:30 -0500

    Popup Changes

--- Assets/Scripts/Managers/PopupManager.cs
+++ Assets/Scripts/Managers/PopupManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:205b5ea01a327d17d02da52fc698af8f254c0afabb62168661256cf2e7c5ffcd
-size 5604
+oid sha256:f54255571d76e4e39f254c4113273a687beb5309e27051a90a584e50975b0063
+size 5670
