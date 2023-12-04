a0aae249ade3bacc92ac1e2dccae39137fdd4258
LagSpikeStorm <112224836+LagSpikeStorm@users.noreply.github.com>
2023-12-03 20:58:40 -0500

    Added Death Anim for the enemysprites

--- Assets/Scripts/Gameplay/Note.cs
+++ Assets/Scripts/Gameplay/Note.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5e5dfdb3d411d16406c3d7aec090930240dc3f313689391211ffe80deed3ffe
-size 1972
+oid sha256:1e99407ebd6d51855524e1acd2f659b8cb009a74c09732ccdfb5a19161eedf87
+size 2608
